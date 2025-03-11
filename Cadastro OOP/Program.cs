f5ed91b94efe989a98aa998304634a20889c06d3
Douglas-Perez <douglas.perez.c@hotmail.com>
2025-03-11 19:13:17 -0300

    feat: adicionar a funcionalidade de cadastro de aluno novo

--- Cadastro OOP/Program.cs
+++ Cadastro OOP/Program.cs
@@ -5,16 +5,26 @@
         static void Main(string[] args)
         {
             Terminal terminal = new();
+            int matricula = 0;
+            List<Aluno> alunos = new();
+                Console.WriteLine("Bem vindo ao nosso sistema de cadastro!\nO que gostaria de fazer?\n");
             while (true)
             {
-                Console.WriteLine("Bem vindo ao nosso sistema de cadastro!\nO que gostaria de fazer?\n");
                 switch (terminal.Menu())
                 {
-                    case 1:
+                    case 1: //lista
                         break;
-                    case 2:
+                    case 2: //cadastrar
+                        Console.WriteLine("Digite o nome do aluno");
+                        string nome = Console.ReadLine();
+                        Console.WriteLine("Digite o E-mail do aluno");
+                        string email = Console.ReadLine();
+                        Console.WriteLine("Digite o Curso do aluno");
+                        string curso = Console.ReadLine();
+                        matricula++;
+                        alunos.Add(new Aluno(nome,email,curso,matricula));
                         break;
-                    case 3:
+                    case 3: //sair
                         return;
                 }
             }
