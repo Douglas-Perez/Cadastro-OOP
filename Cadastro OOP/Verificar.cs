8cdc498e4afa3865a4a79f14b9ae7ad128bbfe7f
Douglas-Perez <douglas.perez.c@hotmail.com>
2025-03-11 21:58:52 -0300

    feat: adicionar metodo Nome() para fazer a verificação do nome do aluno

--- Cadastro OOP/Verificar.cs
+++ Cadastro OOP/Verificar.cs
@@ -8,5 +8,17 @@ namespace Cadastro_OOP
 {
     class Verificar
     {
+        Terminal terminal = new();
+        public string Nome(string nome)
+        {
+            while (nome.Length == 0)
+            {
+                Console.Clear();
+                terminal.MensagemErro();
+                Console.WriteLine("Digite o nome do aluno");
+                nome = Console.ReadLine();
+            }
+            return nome;
+        }
     }
 }
